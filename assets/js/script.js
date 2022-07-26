44b4eb6899d68b2c6fc20c86b9f6b253c5b061ab
VoravichS <vsilapac@gmail.com>
2022-07-26 18:57:54 -0500

    results screen and high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,9 @@ var textEl = document.querySelector('.text-content');
 var startButtonEl = document.querySelector('.start-button');
 var multiButtonEl = document.querySelector('.multi-button');
 var choiceButtonsEl = document.querySelector('.choice-buttons');
-var button1El = document.querySelector('#button-1');
-var button2El = document.querySelector('#button-2');
-var button3El = document.querySelector('#button-3');
-var button4El = document.querySelector('#button-4');
-var answer1El = document.querySelector('#answer-1');
-var answer2El = document.querySelector('#answer-2');
-var answer3El = document.querySelector('#answer-3');
-var answer4El = document.querySelector('#answer-4');
-
+var feedbackEl = document.querySelector('.quiz-feedback');
+var inputEl = document.querySelector(".input-group");
+var submitBtnEl = document.querySelector("#submit-btn");
 
 questions = 
     ["AAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
@@ -29,6 +23,7 @@ answers =
     ["E","F","G","H"]];
 var questionOrder = [0,1,2,3,4];
 var answerOrder = [0,1,2,3];
+var correctAnswer = 0;
 var currentQuestion = 0;
 var seconds = 0;
 
@@ -43,10 +38,9 @@ startButtonEl.addEventListener('click', function () {
     // remove the start button and title, display multiple choice, show a new question
     titleEl.setAttribute("style","display: none");
     startButtonEl.setAttribute("style","display: none");
-    button1El.setAttribute("style","display: block");
-    button2El.setAttribute("style","display: block");
-    button3El.setAttribute("style","display: block");
-    button4El.setAttribute("style","display: block");
+    for (var i = 0; i < 4; i++) {
+        choiceButtonsEl.children[i].setAttribute("style","display: block");
+    }
     newQuestion();
 
     // set text of timer to starting value
@@ -54,7 +48,7 @@ startButtonEl.addEventListener('click', function () {
  
     // start the countdown
     countdown = setInterval(function () {
-      seconds = seconds - 1;
+      seconds--,
       timerEl.textContent = seconds;
   
       // game over if seconds = 0
@@ -67,28 +61,43 @@ startButtonEl.addEventListener('click', function () {
 function newQuestion() {
     if (currentQuestion > questions.length-1) {
         endGame();
+        return;
     }
-    answerOrder = [0,1,2,3];
     shuffle(answerOrder);
     // Set text of current question
     textEl.textContent = questions[questionOrder[currentQuestion]];
 
     // Set answers of current question, if the randomized index of current question is 0, then set it as the correct answer
     for (var i = 0; i < answerOrder.length; i++) {
-        var currentButton = eval("button" + (i+1) + "El");
-        var currentAnswer = eval("answer" + (i+1) + "El");
+        var currentButton = choiceButtonsEl.children[i];
+        var currentAnswer = choiceButtonsEl.children[i].children[0];
+        // if the current answer in the order is the correct one 
+        // (originally index 0 before random), set the answer attr to right
         if (answerOrder[i] === 0) {
             currentButton.setAttribute("answer","right");
+            // Store the correct button for later use
+            correctAnswer = currentButton;
         }
         currentAnswer.textContent = answers[questionOrder[currentQuestion]][answerOrder[i]];
     }
 }
 
 function endGame() {
+    // stop timer
     clearInterval(countdown);
-    seconds = 0;
-    currentQuestion = 0;
-    questionOrder = [0,1,2,3,4];
+    
+    // end screen text
+    titleEl.setAttribute("style","display: block");
+    titleEl.textContent = "All Done!";
+    textEl.textContent = "Your final score was: " + seconds;
+    
+    // remove multiple choice buttons
+    for (var i = 0; i < 4; i++) {
+        choiceButtonsEl.children[i].setAttribute("style","display: none");
+    }
+
+    // show input initials
+    inputEl.setAttribute('style',"display:block");
 }
 
 // Implementing Fisher-Yates Shuffle, courtesy of 
@@ -107,33 +116,58 @@ function shuffle(array){
 
 choiceButtonsEl.addEventListener('click', function(event) {
     let clickedButton = event.target;
-    // Find the button if span is clicked
-    clickedButton = clickedButton.closest('button');
 
+    // Find the button if span is clicked
+    if (clickedButton.matches("span")) {
+        clickedButton = clickedButton.closest('button');
+    } 
+    
+    // Makes sure to only proceed if a button is clicked and detected 
     if (clickedButton.matches("button")) {
         var chosenAnswer = clickedButton.getAttribute("answer");
         console.log(chosenAnswer);
-        
         if (chosenAnswer === "right") {
             clickedButton.setAttribute('answer','wrong');
             currentQuestion++;
+            showFeedback("Correct!");
+            newQuestion();
+        } else if (chosenAnswer === "wrong"){
+            correctAnswer.setAttribute('answer','wrong');
+            currentQuestion++;
+            showFeedback("Wrong!");
             newQuestion();
         }
     } 
-});
+})
 
-// initialize starting variables
-//      set the initial starting seconds
-//      display those seconds
-// start countdown
-//      interval function that counts seconds down
-// game over when seconds = 0
-//      gameOver function
-// display question and multiple choice buttons
-//      newQuestion function
-//          replace content of text-content with the question
-//          show the multi buttons
-//          set the right answer
-// click handler for multi choice buttons to progress the game
-//      display right or wrong depending on question
-//      renderQuestion function again
+function showFeedback(string) {
+    // set text
+    feedbackEl.textContent = string;
+    
+    // set the feedback opacity to 1
+    feedbackEl.setAttribute("style","opacity: 1");
+
+    // then, after 1 second, set it back to 0
+    var feedbackSeconds = 10;
+    feedbackTimer = setInterval(function () {
+        feedbackSeconds--;
+        if (feedbackSeconds === 0) {
+            feedbackEl.setAttribute("style","opacity: 0");
+            clearInterval(feedbackTimer);
+        }
+      }, 100);
+}
+
+submitBtnEl.addEventListener('click', function (event) {
+    var initials = document.querySelector('#initials').value;
+    if (initials == '') {
+        showFeedback("Please enter initials to save highscore.");
+    } else {
+        localStorage.setItem('initials',initials);
+        localStorage.setItem('score',seconds);
+    }  
+})
+
+
+
+
